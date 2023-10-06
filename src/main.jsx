b8ca689afc6cc8a382a8ef0fb6fa7c4e3d30524e
Aline Prasser <aline.prasser@codenapp.com>
2023-10-05 23:02:57 -0300

    refactoring: providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CartProvider } from "./contexts/CartContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import Providers from "./providers/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
